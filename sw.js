3ef7f8ec1d4ad5a3f1824fc84939c14262b8c327
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-02-24 12:22:39 +0100

    isak

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+    '/',
+    'main.css',
+    'test.js',
+    'index.html'
+    ];
+
+    self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+    });
+  });
