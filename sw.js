02c174c3b6303e6653a2a212e79aaede97354ca1
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-03-04 14:07:53 +0100

    isakpc

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 var CACHE_NAME = 'my-site-cache-v1'
 var urlsToCache = [
   '/',
-  'main.css',
-  'initializeSW.js',
-  'index.html',
-  'index.html?homescreen=1',
-  'manifest.json',
-  'sw.js',
-  'content.js',
-  'homescreen192.png',
-  'maskable_homescreen192.png',
-  'fonts/OpenSans-Regular.ttf',
-  'fonts/Nunito-Regular.ttf'
+  '/main.css',
+  '/initializeSW.js',
+  '/index.html',
+  '/index.html?homescreen=1',
+  '/manifest.json',
+  '/sw.js',
+  '/content.js',
+  '/homescreen192.png',
+  '/maskable_homescreen192.png',
+  '/fonts/OpenSans-Regular.ttf',
+  '/fonts/Nunito-Regular.ttf'
 ]
 
 self.addEventListener('install', function(event) {
