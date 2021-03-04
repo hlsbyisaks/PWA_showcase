39be34e401eb68d45c49580048874be31bc39739
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-03-04 13:45:07 +0100

    isakpc

--- slask.js
+++ slask.js
@@ -50,7 +50,7 @@ $('.button').on("click", function(e) {  /* click function for NEXT button */
     }) */
 
     /* OLD install */
-    //self.addEventListener('install', function(event) {
+    /* self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -59,4 +59,39 @@ $('.button').on("click", function(e) {  /* click function for NEXT button */
             return cache.addAll(urlsToCache)
         })
     )
-    //});
+    }); */
+
+    /* OLD SW */
+    /* self.addEventListener('install', function(event) {
+        var CACHE_NAME = 'my-site-cache-v1'
+        var urlsToCache = [
+        '/',
+        'main.css',
+        'initializeSW.js',
+        'index.html',
+        'manifest.json',
+        'sw.js',
+        'content.js',
+        'homescreen192.png',
+        'maskable_homescreen192.png',
+        'fonts/.'
+        ]
+    
+        event.waitUntil(
+          caches.open(CACHE_NAME)
+          .then(function(cache) {
+              console.log('Opened cache')
+              return cache.addAll(urlsToCache)
+          })
+        )
+      })
+    
+      self.addEventListener('fetch', function(event) {
+        console.log(event.request.url);
+       
+        event.respondWith(
+          caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+          })
+        );
+       }); */
--- sw.js
+++ sw.js
@@ -1,33 +1,35 @@
 self.addEventListener('install', function(event) {
-    var CACHE_NAME = 'my-site-cache-v1'
-    var urlsToCache = [
+  var CACHE_NAME = 'my-site-cache-v1'
+  var urlsToCache = [
     '/',
     'main.css',
     'initializeSW.js',
     'index.html',
+    'index.html?homescreen=1',
     'manifest.json',
     'sw.js',
     'content.js',
     'homescreen192.png',
     'maskable_homescreen192.png',
-    'fonts/.'
-    ]
+    'fonts/OpenSans-Regular.ttf',
+    'fonts/Nunito-Regular.ttf'
+  ]
 
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-      .then(function(cache) {
-          console.log('Opened cache')
-          return cache.addAll(urlsToCache)
-      })
-    )
-  })
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+        console.log('Opened cache')
+        return cache.addAll(urlsToCache)
+    })
+  )
+})
 
-  self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-   });
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
