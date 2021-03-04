9415c9d040f9e2d107a4e4e69ba9fe61a98a559d
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-03-04 14:34:56 +0100

    isakpc

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-var CACHE_NAME = 'my-site-cache-v1'
+var CACHE_NAME = 'PWA_showcase_cache-v1'
 var urlsToCache = [
-  '/',
+  '.',
   'main.css',
-  'initializeSW.js',
   'index.html',
-  'index.html?homescreen=1',
-  'manifest.json',
-  'content.js',
-  'homescreen192.png',
-  'maskable_homescreen192.png',
-  'fonts/OpenSans-Regular.ttf',
-  'fonts/Nunito-Regular.ttf'
 ]
 
 self.addEventListener('install', function(event) {
@@ -23,12 +15,30 @@ self.addEventListener('install', function(event) {
   )
 })
 
-/* self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
- 
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
     })
   );
-}); */
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
