15e31539e5b97e5b8d2d9dc39902038014bdbe0d
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-03-15 15:33:40 +0100

    isakpc

--- content.js
+++ content.js
@@ -1,53 +1,22 @@
-let contentArr = [ /* obj containing text content */
-    {
-        h1: "Welcome",
-        h2: "What is a PWA?",
-        p: "A PWA (Progressive Web App) is a web based app, however, unlike a traditional web app it supports installation to home screen, offline use and push notifications. Just like you would expect from a regular app. Stick around and try it out!"
-    },
-    {
-        h1: "Introduction",
-        h2: "Purpose",
-        p: "The PWA concept opens up for some pretty sizeable advantages regarding distribution of apps, although useless if the installation process is to cumbersome. The purpose of this app is to research the impact of said installation process. This PWA will show some core functionalites of the PWA concept, as well as guide through the installation process."
-    },
-    {
-        h1: "Installation",
-        h2: "How do I install a PWA?",
-        p: 'If you are using Google Chrome right now, good news, there should be a prompt at the bottom of your screen, asking if you want to add this app to your homescreen. <br><br> If you are on any other browser, there is no installation prompt in clear sight. You need to navigate to the browsers alternative menu, scroll down a bit, and look for "Add to homescreen" and manually name it. <br><br>Continue for examples.'
-    },
-    {    
-        h1: "Android",
-        h2: "Installation guide",
-        p: '1. Press the alternatives button',
-        img1: 'images/install_android_1.jpg',
-        p2:'2. Navigate to, and press, "Add to Home screen". You might need to scroll down.',
-        img2: 'images/install_android_2.jpg',
-    },
-    {    
-        h1: "Android",
-        h2: "Installation guide",
-        p:'3. Add a name and continue.',
-        img1: 'images/install_android_3.jpg'
-    },
-    {    
-        h1: "Android",
-        h2: "Installation guide",
-        p:'The installation is complete, open this app via your home screen for a fullscreen experience.',
-    },
-    {    
-        h1: "Thank you",
-        h2: "for participating",
-        p:'I would greatly appreciate if you continue and describe your experience of the installation process in the survey linked below.',
-        img1: '',
-        p2:'Recap of PWA core functionalities: <br>1. Offline use. <br>2. Installation to homescreen. <br>3. Push notifications',
-        img2: ''
-    }
-]
 
-let contentCount = 0
+let contentCount
+if(localStorage.getItem('currentPage') == null) {
+    contentCount = 0
+    localStorage.setItem('currentPage', contentCount)
+} 
+
+
+if (navigator.userAgent.includes("Android")) { contentArr = contentAndroid } //Checks user OS for appropriate install guide
+else { contentArr = contentIos}
+nextContent(contentArr, "Prev")
+
 function nextContent(contentArr, buttonId) {  /* Swapping content of page using content object */
+    contentCount = parseInt(localStorage.getItem('currentPage'))
     if (buttonId == "Next") {                   /* Also manages navigation buttons */
         if (contentCount < contentArr.length-1) {
+            
             contentCount += 1
+            localStorage.setItem('currentPage', contentCount)
             $('.prev').css({
                 'border-color': 'var(--text-color)',
                 'color': 'var(--text-color)'
@@ -58,7 +27,10 @@ function nextContent(contentArr, buttonId) {  /* Swapping content of page using 
             })
         }
     } else {
-        if (contentCount > 0) {contentCount -= 1}
+        if (contentCount > 0) {
+            contentCount -= 1
+            localStorage.setItem('currentPage', contentCount)
+        }
         if (contentCount == 0) {
             $('.prev').css({
                 'border-color': 'var(--background-color)',
@@ -98,7 +70,7 @@ function nextContent(contentArr, buttonId) {  /* Swapping content of page using 
     if (contentCount == contentArr.length-1) {
         $('.navBtn').show()
         $('.button').hide()
-        $('.nav').hide()
+        $('.dotContainer').hide()       
     }
 
     showSlides(contentCount)  /* For dots visual navigation */
@@ -106,6 +78,9 @@ function nextContent(contentArr, buttonId) {  /* Swapping content of page using 
 }
 
 $('.button').on("click", function(e) {  /* click function for NEXT button */
+    if (navigator.userAgent.includes("Android")) { contentArr = contentAndroid } //Checks user OS for appropriate install guide
+    else { contentArr = contentIos}
+
     nextContent(contentArr, $(this).data('id'))
     e.preventDefault()
 
@@ -194,4 +169,5 @@ $('.iosButton').on("click", function(e) {
     $('.navBtn').hide()
     $('.button').show()
     $('.nav').show()
+    $('.dotContainer').show()
 })
---
+++ content_ios.js
@@ -0,0 +1,44 @@
+let contentIos = [ /* obj containing text content */
+    {
+        h1: "Welcome",
+        h2: "What is a PWA?",
+        p: "A PWA (Progressive Web App) is a web based app, however, unlike a traditional web app it supports installation to home screen, offline use and push notifications. Just like you would expect from a regular app. Stick around and try it out!"
+    },
+    {
+        h1: "Introduction",
+        h2: "Purpose",
+        p: "The PWA concept opens up for some pretty sizeable advantages regarding distribution of apps, although useless if the installation process is to cumbersome. The purpose of this app is to research the impact of said installation process. This PWA will show some core functionalites of the PWA concept, as well as guide through the installation process."
+    },
+    {
+        h1: "Installation",
+        h2: "How do I install a PWA?",
+        p: 'If you are using Google Chrome right now, good news, there should be a prompt at the bottom of your screen, asking if you want to add this app to your homescreen. <br><br> If you are on any other browser, there is no installation prompt in clear sight. You need to navigate to the browsers alternative menu, scroll a bit, and look for "Add to Home Screen" and manually name it. <br><br>Continue for examples.'
+    },
+    {    
+        h1: "iOS",
+        h2: "Installation guide",
+        p: '1. From the bottom of your screen, swipe up to reveal the alternatives menu.',
+        img1: 'images/install_ios_1.jpg',
+        p2:'2. Navigate to, and press, "Add to Home screen". You might need to scroll.',
+        img2: '',
+    },
+    {    
+        h1: "iOS",
+        h2: "Installation guide",
+        p:'3. Add a name and continue.',
+        img1: 'images/install_android_3.jpg'
+    },
+    {    
+        h1: "iOS",
+        h2: "Installation guide",
+        p:'The installation is complete, open this app via your home screen for a fullscreen experience.',
+    },
+    {    
+        h1: "Thank you",
+        h2: "for participating",
+        p:'I would greatly appreciate if you continue and describe your experience of the installation process in the survey linked below.',
+        img1: '',
+        p2:'Recap of PWA core functionalities: <br>1. Offline use. <br>2. Installation to homescreen. <br>3. Push notifications',
+        img2: ''
+    }
+]
