816ad737b0d83bd9915f9b8b8831f78a1e9918dd
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2021-03-26 11:47:51 +0100

    isak

--- content_ios.js
+++ content_ios.js
@@ -2,22 +2,26 @@ let contentIos = [ /* obj containing text content */
     {
         h1: "Welcome",
         h2: "Who am I & what is this?",
-        p: "I am a student at the faculty of Media Technology at Malmö University studying web development. As a part of my bachelors thesis i developed this app to showcase core functionalities of a concept called <i>progressive web apps</i> which simplifies a lot of app development in terms of distribution and platform adaptation."
+        p: "I am a student at the faculty of Media Technology at Malmö University, studying web development. As a part of my bachelors thesis i developed this app to showcase core functionalities of a concept called progressive web apps which simplifies a lot of app development in terms of distribution and platform adaptation.",
+        btn: 'What is a PWA<div class="buttonAnim"><i class="arrow right"></i><i class="arrow right right2"></i>'
     },
     {
         h1: "",
         h2: "What is a PWA?",
-        p: "A PWA (Progressive Web App) is a web based app, opposed to a native app being installed on the platforms operating system. Traditionally, web apps lacked support for certain key functionalities of app development. That is why we today see PWAs which support installation to home screen, offline use and push notifications. Just like you would expect from a native app. These advantages makes PWA a viable choice for many businesses. Other advantages are: low space requirements, availability in areas with poor internet coverage and independent of platform specific app marketplaces with associated sales fees. <br><br>Stick around and try it out!"
+        p: "A PWA (Progressive Web App) is a web based app, opposed to a native app being installed on the platforms operating system. Traditionally, web apps lacked support for certain key functionalities of app development. That is why we today see PWAs which support installation to home screen, offline use and push notifications. Just like you would expect from a native app. These advantages makes PWA a viable choice for many businesses. Other advantages are: low space requirements, availability in areas with poor internet coverage and independent of platform specific app marketplaces with associated sales fees. <br>Stick around and try it out!",
+        btn: 'Purpose<div class="buttonAnim"><i class="arrow right"></i><i class="arrow right right2"></i>'
     },
     {
         h1: "Introduction",
         h2: "Purpose",
-        p: "The PWA concept opens up for some pretty sizeable advantages regarding distribution of apps, although useless if the installation process is to cumbersome. The purpose of this app is to research the impact of the installation process, together with a survey. This PWA will show some core functionalites of the PWA concept, as well as guide through the installation process."
+        p: "The PWA concept opens up for some pretty sizeable advantages regarding distribution of apps, although useless if the installation process is to cumbersome. The purpose of this app is to research the impact of the installation process, together with a survey. This PWA will show some core functionalites of the PWA concept, as well as guide through the installation process.",
+        btn: 'Guide<div class="buttonAnim"><i class="arrow right"></i><i class="arrow right right2"></i>'
     },
     {
         h1: "Installation",
         h2: "How do I install a PWA?",
-        p: 'If you are using Google Chrome right now, good news, there should be a prompt at the bottom of your screen, asking if you want to add this app to your homescreen. <br><br> If you are on any other browser, there is no installation prompt in clear sight. You need to navigate to the browsers alternative menu, scroll a bit, and look for "Add to Home Screen" and manually name it. <br><br>Continue for examples.'
+        p: 'If you are using Google Chrome right now, good news, there should be a prompt at the bottom of your screen, asking if you want to add this app to your homescreen. <br><br> If you are on any other browser, there is no installation prompt in clear sight. You need to navigate to the browsers alternative menu, scroll down a bit, and look for "Add to homescreen" and manually name it. <br><br>Continue for examples.',
+        btn: 'Next<div class="buttonAnim"><i class="arrow right"></i><i class="arrow right right2"></i>'
     },
     {    
         h1: "iOS",
